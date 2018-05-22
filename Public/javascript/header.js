d8eeb8d9b545e6f4c41fb9626da0fcc3841babe0
Scott <pravaca22@gmail.com>
2018-05-22 11:21:02 -0700

    First commit

---
+++ Public/javascript/header.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
+});
+
+var windowWidth = $(window).width();
+if (windowWidth < 992) {
+    $("hr.hidden").addClass("visible");
+    $("div.hidden").addClass("visible");
+    $("div.hidden").removeClass("hidden");
+}
+else {
+    $("div.hidden").removeClass("visible");
+    $("hr.visible").addClass("hidden");
+    $("div.visible").addClass("hidden");
+}
+ 
+$(window).resize(function () {
+    var windowWidth = $(window).width();
+    if (windowWidth < 992) {
+        $("hr.hidden").addClass("visible");
+        $("div.hidden").addClass("visible");
+        $("div.hidden").removeClass("hidden");
+    }
+    else {
+        $("div.hidden").removeClass("visible");
+        $("hr.visible").addClass("hidden");
+        $("div.visible").addClass("hidden");
+    }
+});
+
+$(document).ready(function () {
+    var links = $('.navbar ul li a');
+    $.each(links, function (key, va) {
+        if (va.href == document.URL) {
+            $(this).addClass('active');
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.chart__bar').click(function () {
+        var option = $(this).attr('id');
+        $('.charts').find(".click--me").removeClass('click--me');
+        $('.desc').find(".visible").fadeOut("500", function(){
+            $(this).removeClass('visible').addClass('hidden');
+            $('.desc').find("." + option).fadeIn("500", function(){
+                $(this).addClass("visible").removeClass("hidden");
+            })
+        })
+    })
+});
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $(".chart__bar a, a.go-up").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+$(document).ready(function () {
+    $("#phone").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+});
+
+
+
---
+++ app.js
@@ -0,0 +1,101 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    flash = require('connect-flash'),
+    nodemailer = require("nodemailer");
+
+var app = express();
+
+mongoose.connect("mongodb://admin:!1Mlab!1@ds119930.mlab.com:19930/portfolio_scott");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(require("express-session")({
+    secret: "Zelda is a cutie",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(function(req, res, next){
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+app.get("/portfolio", function(req, res){
+    res.render("portfolio");
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+app.get("/resources", function(req, res){
+    res.render("resources");
+})
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+})
+
+app.post('/contact', function(req, res){
+    var output = `
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Company: ${req.body.companyName}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Phone: ${req.body.phone}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.details}</p>
+    `
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'inquery.selke@gmail.com', // generated ethereal user
+            pass: 'sselke123'  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <inquery.selke@gmail.com>', // sender address
+        to: 's.w.selke@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+            req.flash("error", "Error Sending Email, Please Try Again");
+            res.redirect('contact');
+        } else {
+            req.flash("success", "Email Sent!");
+            res.redirect('contact');
+        }
+    });
+})
+
+//
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started...");
+})
